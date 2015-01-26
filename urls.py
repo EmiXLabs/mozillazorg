ed2f8c94e8d199abde44325c350d5339e8b494fc
Emin Mastizada <emin25@ovi.com>
2015-01-26 03:46:50 +0200

    Signed-off-by: Emin Mastizada <emin25@ovi.com>

--- urls.py
+++ urls.py
@@ -37,7 +37,7 @@ sitemaps = {
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     ("^admin/", include(admin.site.urls)),
