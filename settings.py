a17ef83abe8a3bf738b6d38a4e296529ec8a9e1d
Emin Mastizada <emin25@ovi.com>
2015-02-21 23:43:35 +0200

    enable compress

--- settings.py
+++ settings.py
@@ -74,6 +74,7 @@ from __future__ import absolute_import, unicode_literals
 # Setting to turn on featured images for blog posts. Defaults to False.
 #
 BLOG_USE_FEATURED_IMAGE = True
+COMPRESS_ENABLED = True
 
 # If True, the south application will be automatically added to the
 # INSTALLED_APPS setting.
